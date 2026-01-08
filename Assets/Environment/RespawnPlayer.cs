72c93191fd2b5560a419f4bba3b75d5a46a896dd
Billy Harrold <Billy2.Harrold@live.uwe.ac.uk>
2026-01-08 13:13:50 +0000

    Death & respawn

---
+++ Assets/Environment/RespawnPlayer.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnPlayer : MonoBehaviour
+{
+    [Header("References")]
+    public Transform spawnPoint;
+    public CharacterController controller;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player"))
+        {
+            return;  
+        }
+
+        controller.enabled = false;
+        Debug.Log("Death");
+        controller.transform.position = spawnPoint.position;
+        controller.enabled = true;
+    }
+}
