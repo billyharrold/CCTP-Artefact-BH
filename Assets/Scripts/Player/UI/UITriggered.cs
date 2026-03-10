163d6c96d1b9eabab44ad012183dcd9b399588db
Billy Harrold <Billy2.Harrold@live.uwe.ac.uk>
2026-03-10 16:09:57 +0000

    UI trigger that pops up input UI

---
+++ Assets/Scripts/Player/UI/UITriggered.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+
+public class UITriggered : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public PopUpContent content;
+
+    public GameObject popUpPanel;
+
+    [SerializeField] private Image icon;
+
+    void Awake()
+    {
+        popUpPanel.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        popUpPanel.SetActive(true);
+        icon.sprite = content.UIicon;
+        popUpPanel.GetComponentInChildren<TextMeshProUGUI>().text = content.prompt;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        popUpPanel.SetActive(false);
+    }
+
+}
