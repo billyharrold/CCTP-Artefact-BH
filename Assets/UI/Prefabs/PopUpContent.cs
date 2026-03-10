d796b97257392a184d32b79496e0bffc6109bad7
Billy Harrold <Billy2.Harrold@live.uwe.ac.uk>
2026-03-10 15:53:10 +0000

    Imported UI - making UI panels

---
+++ Assets/UI/Prefabs/PopUpContent.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "PopUpContent", menuName = "Scriptable Objects/PopUpContent")]
+public class PopUpContent : ScriptableObject
+{
+    public Sprite UIicon;
+    public string prompt;
+
+}
