bf6f05c1a48b2b2f1b9e5c44d4cedcdf27d01aca
Billy Harrold <Billy2.Harrold@live.uwe.ac.uk>
2026-03-23 15:08:02 +0000

    WIP new rolling eval - every 5 seconds

--- Assets/Scripts/Player/Data/DataEvaluator.cs
+++ Assets/Scripts/Player/Data/DataEvaluator.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.Serialization;
 
@@ -7,11 +8,16 @@ public class DataEvaluator : MonoBehaviour
     // uses scriptable objects to define rules for data weighting and classification.
 
    [SerializeField] private PlatformSkillRules DefaultskillModel;
-    
+
+   private int rollingDeaths;
+   private float rollingTime;
 
 
     private int deaths;
     private float elapsedTime;
+
+    private float evalTimer;
+    [SerializeField] private float interval = 3f;
 
     // spelt this function wrong - FIX
     public PlayerData EvaluatePlayerData(PlatformSkillRules skillModel)
@@ -41,5 +47,25 @@ public class DataEvaluator : MonoBehaviour
 
         PlayerData newData = EvaluatePlayerData(model);
         PlayerManager.Instance.UpdatePlayerData(newData);
+    }
+
+
+    private void Update()
+    {
+        rollingTime += Time.deltaTime;
+        evalTimer += Time.deltaTime;
+
+        if (evalTimer >= interval)
+        {
+            Debug.Log("Eval occuring");
+            ApplyNewSkillData();
+            evalTimer = 0f;
+        }
+    }
+
+    public void TrackDeath()
+    {
+        rollingDeaths++;
+        
     }
 }
